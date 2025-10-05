f0bf9a961444afdbd5b9573d183eaade9e0ba6b1
vctrubio <vctrubio@gmail.com>
2025-10-05 16:00:00 +0200

    layout wtih cover

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,14 +35,16 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
+      { url: "/logo-tkh.png" },
       { url: "/icons/icon-192x192.png", sizes: "192x192", type: "image/png" },
       { url: "/icons/icon-512x512.png", sizes: "512x512", type: "image/png" },
     ],
     apple: [
+      { url: "/logo-tkh.png" },
       { url: "/icons/icon-152x152.png", sizes: "152x152", type: "image/png" },
       { url: "/icons/icon-192x192.png", sizes: "192x192", type: "image/png" },
     ],
-    shortcut: "/icons/icon-192x192.png",
+    shortcut: "/logo-tkh.png",
   },
   other: {
     "mobile-web-app-capable": "yes",
