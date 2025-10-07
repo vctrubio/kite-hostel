d24c76efd09386479d9a0797dbb549eee9ba2103
vctrubio <vctrubio@gmail.com>
2025-10-07 21:49:04 +0200

    build with unused vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,14 +13,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        {
-          "argsIgnorePattern": "^_",
-          "varsIgnorePattern": "^_",
-          "destructuredArrayIgnorePattern": "^_"
-        }
-      ],
+      "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "react/no-unescaped-entities": "off",
       "react/jsx-no-comment-textnodes": "off",
